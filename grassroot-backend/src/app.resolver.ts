8507eed2346bf70ff314a9d926b9b748c3485b55
Prince Anuragi <princanurag07@gmail.com>
2022-12-11 19:06:35 +0530

    fixes

--- grassroot-backend/src/app.resolver.ts
+++ grassroot-backend/src/app.resolver.ts
@@ -1,10 +1,4 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 
 @Resolver()
-export class AppResolver {
-  @Mutation(() => String)
-  getNonce(address: String): string {
-    return "nonce";
-  }
-  
-}
+export class AppResolver {}
